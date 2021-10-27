45c1cfe1183c29cfb0a8d83cd8dd3c2ec9672cd4
Sephyr3s <seumamu@gmail.com>
2021-10-27 13:32:13 -0500

    ChasgesForm

---
+++ my-app/src/componentes/ProductForm.js
@@ -0,0 +1,41 @@
+import React,{useState} from "react";
+
+
+export default function itemForm(props){
+    const estadoIncialForm = {
+        nombre:'',
+        precio:''
+    };
+
+    const [estadoForm, cambiarEstadoForm] = useState(estadoIncialForm);
+
+    const gestionarCamposForm = event => {
+        const {name,value} = event.target;
+        cambiarEstadoForm({...estadoForm,[name]:value});
+    }
+
+    const onSubmitForm = ev =>{
+        ev.preventDefault();
+        console.log(estadoForm);
+        props.agregar(estadoForm);
+
+        cambiarEstadoForm(estadoIncialForm);
+
+    }
+
+    return (
+        <form onSubmit={onSubmitForm}>
+           <div className="mb-3">
+                <label htmlFor="ProductName" className="form-label">Nombre del producto</label>
+                <input type="text" name="nombre" className="form-control" id="ProductName" value={estadoForm.nombre} onChange={gestionarCamposForm}/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="precio" className="form-label">precio</label>
+                <input type="number" name="Precio" className="form-control" id="precio" value={estadoForm.precio}  onChange={gestionarCamposForm}/>
+            </div>
+            <div className="mb-3">
+                <button type="submit" className="btn btn-primary">Agregar Producto</button>
+            </div>
+        </form>
+    );
+}
